7ca8245cc8758766a5af61e8378173e82872945e
Victória César <victoriacesaras@gmail.com>
2025-02-15 20:19:31 -0300

    feat: add script inside page

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -3,8 +3,11 @@ import { Header } from './components/header';
 import { PostsSection } from './components/posts-section';
 import { ProjectsSection } from './components/projects-section';
 import { UserSection } from './components/user-section';
+import useBtnExtractData from './hooks/useBtnExtractData';
 
 function App() {
+  useBtnExtractData();
+
   return (
     <div className="flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="w-full max-w-[720px] mx-auto bg-white dark:bg-zinc-800">
---
+++ page/src/hooks/useBtnExtractData.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const useBtnExtractData = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '../public/plugin.js';
+    script.async = true;
+
+    script.onload = () => {
+      console.log('Plugin carregado com sucesso!');
+    };
+
+    script.onerror = () => {
+      console.error('Erro ao carregar o plugin.');
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+};
+
+export default useBtnExtractData;
